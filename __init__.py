54818d3d792215ea59fae9cb51ff35819ec0a232
Apophis-2020 <jjttkbford@yahoo.com>
2025-02-23 16:31:17 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,31 +1,7 @@
-"""
-The "hello world" custom component.
+DOMAIN = "homeassistant_perplexity"
 
-This component implements the bare minimum that a component should implement.
+async def async_setup(hass, config):
+    hass.states.async_set("hello_state.world", "Paulus")
 
-Configuration:
-
-To use the hello_world component you will need to add the following to your
-configuration.yaml file.
-
-hello_world_async:
-"""
-from __future__ import annotations
-
-import asyncio
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-# The domain of your component. Should be equal to the name of your component.
-DOMAIN = "hello_world_async"
-
-
-@asyncio.coroutine
-def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Setup our skeleton component."""
-    # States are in the format DOMAIN.OBJECT_ID.
-    hass.states.async_set('hello_world_async.Hello_World', 'Works!')
-
-    # Return boolean to indicate that initialization was successfully.
+    # Return boolean to indicate that initialization was successful.
     return True
