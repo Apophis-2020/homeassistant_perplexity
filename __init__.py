d128fe4868a8b75aafa76612cd5750985c694a12
Apophis-2020 <jjttkbford@yahoo.com>
2025-02-23 16:47:31 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ DOMAIN = "homeassistant_perplexity"
 
 async def async_setup(hass, config):
 
-    print("Hopefully this gets printed to the logs")
+    # print("Hopefully this gets printed to the logs")
 
     # Return boolean to indicate that initialization was successful.
     return True
